596e54504696b9ec34227e6c084253e525d33583
Kamal Dasik <kamal@Kamals-MacBook-Air.local>
2026-05-25 23:04:35 +0530

    Added schemas

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -5,8 +5,8 @@ class ApiResponse {
         message = "Success"
     ){
         this.statusCode = statusCode
-        this.data = data,
-        this.message = message,
+        this.data = data
+        this.message = message
         this.success = statusCode < 400 
     }
 }
